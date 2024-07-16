a0716a8c7ea0e74eb29680d651bd329eac04d5cd
lyssandraa <145362887+lyssandraa@users.noreply.github.com>
2024-07-16 10:32:04 +0100

    feature: add log in route fix: change sucess message for singup

--- src/users/controllers.js
+++ src/users/controllers.js
@@ -1,12 +1,23 @@
-const { req, request } = require("express");
 const User = require("./model");
 
 const signUp = async (req, res) => {
   try {
     const user = await User.create(req.body);
-    res
-      .status(201)
-      .json({ message: "You have signed up successfully", user: user });
+    res.status(201).json({
+      message: "You have signed up successfully",
+      username: user.username,
+    });
+  } catch (err) {
+    res.status(501).json({ message: err.message, err: err });
+  }
+};
+
+const logIn = async (req, res) => {
+  try {
+    res.status(201).json({
+      message: "You have logged in successfully",
+      username: req.user,
+    });
   } catch (err) {
     res.status(501).json({ message: err.message, err: err });
   }
@@ -15,7 +26,7 @@ const signUp = async (req, res) => {
 const getUser = async (req, res) => {
   try {
     const user = await User.findOne({
-      where: { username: req.body.username },
+      where: { username: req.params.username },
     });
     res.status(200).json({ message: "success", user: user });
   } catch (err) {
@@ -32,8 +43,27 @@ const getAllUsers = async (req, res) => {
   }
 };
 
+const updateInfo = async (req, res) => {
+  try {
+    const filterObj = { username: req.body.username };
+    const updateObj = { [req.body.updateKey]: req.body.updateValue };
+
+    await User.update(updateObj, {
+      where: filterObj,
+    });
+
+    const updatedInfo = await User.findOne({ where: filterObj });
+
+    res.status(200).json({ message: "success", updatedInfo: updatedInfo });
+  } catch (err) {
+    res.status(501).json({ message: err.message, err: err });
+  }
+};
+
 module.exports = {
   signUp,
+  logIn,
   getUser,
   getAllUsers,
+  updateInfo,
 };
