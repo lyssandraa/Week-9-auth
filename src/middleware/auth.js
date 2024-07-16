319a7d99b5cb703332254f0abc35e234accfc515
lyssandraa <145362887+lyssandraa@users.noreply.github.com>
2024-07-16 13:37:36 +0100

    feature: add comparePass

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const User = require("../users/model");
 const saltRounds = parseInt(process.env.SALT_ROUNDS);
 // putting a + sign will also convert the string to number //
 
@@ -14,6 +15,29 @@ const hashPass = async (req, res, next) => {
   }
 };
 
+const comparePass = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { username: req.body.username } });
+
+    if (!user) {
+      res.status(401).json({ message: "User not found" });
+    }
+    const passwordVerify = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!passwordVerify) {
+      return res.status(404).json({ message: "Password is incorrect" });
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(500).json({ message: err.message, err });
+  }
+};
+
 module.exports = {
   hashPass,
+  comparePass,
 };
--- src/users/routes.js
+++ src/users/routes.js
@@ -9,13 +9,14 @@ const {
   updateInfo,
 } = require("./controllers");
 
-const { hashPass } = require("../middleware/auth");
+const { hashPass, comparePass } = require("../middleware/auth");
+const { compare } = require("bcrypt");
 
 // POST route to add a user to DB //
 userRouter.post("/", hashPass, signUp);
 
-//
-userRouter.get("/logIn", logIn);
+// POST route to log in //
+userRouter.post("/logIn", comparePass, logIn);
 
 // GET route to read a user from DB by username //
 userRouter.get("/:username", getUser);
